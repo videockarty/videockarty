43b35bda1a51db5bf316b4ff01de961291e36800
videockarty <36168790+videockarty@users.noreply.github.com>
2018-02-06 02:30:49 +0700

    Update mail.js

--- mail.js
+++ mail.js
@@ -7,6 +7,47 @@ module.exports = {
        /* host: 'imap.yandex.ru',
         port: 465,
         secure: true, // true for 465, false for other ports*/
+		service: 'Gmail',
+        auth: {
+            user: 'aivanov19991', // generated ethereal user sozdatel.botov@yandex.ru
+            pass: '1kel9911'  // generated ethereal password 1kel991
+        }
+    });
+
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Телеграм бот" <aivanov19991@gmail.com>', // sender address
+        to: 'valefuwon@kumail8.info', // list of receivers
+        subject: 'Заявка на видеокарту', // Subject line
+        html: body // html body
+    };
+
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        //console.log('Message sent: %s', info.messageId);
+        // Preview only available when sending through an Ethereal account
+        //console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
+        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+    });
+});
+
+	}
+}
+
+/*const nodemailer = require('nodemailer')
+module.exports = {
+	mail: (body)=>{
+		nodemailer.createTestAccount((err, account) => {
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+       /* host: 'imap.yandex.ru',
+        port: 465,
+        secure: true, // true for 465, false for other ports*/
 		service: 'Yandex',
         auth: {
             user: 'videockarty', // generated ethereal user sozdatel.botov@yandex.ru
@@ -38,3 +79,4 @@ module.exports = {
 
 	}
 }
+*/
